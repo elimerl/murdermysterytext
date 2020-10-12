b595a272a668df7bb57dae2504318c0a5ca9d67b
elimerl <32183908+elimerl@users.noreply.github.com>
2020-10-11 19:00:51 -0700

    yeet

---
+++ index.js
@@ -0,0 +1,8 @@
+const classes = require("./lib/classes");
+module.exports = {
+  Player: classes.Player,
+  Map: classes.Map,
+  Location: classes.Location,
+  Game: classes.Game,
+  Item: classes.Item,
+};
